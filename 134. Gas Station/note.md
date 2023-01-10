8f7e97e4e584f4bb97357b6cca25bbd144820393
Mars <mars3397@gmail.com>
2023-01-11 01:27:45 +0800

    20230111

---
+++ 134. Gas Station/note.md
@@ -0,0 +1,3 @@
+### Note
+
+如果從 `ith` gas station 繞不完一圈，有經過的那些 gas station 都不用看了，因為一定到不了，只要看還沒走到的就好了
---
+++ 134. Gas Station/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        for (int i = 0; i < n; i++) {
+            if (gas[i] >= cost[i]) {
+                int store = 0, j = 0; bool flag = true;
+                for (; j < n; j++) {
+                    int next = (i + j) % n;
+                    store += gas[next] - cost[next];
+                    if (store < 0) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    return i;
+                }
+                i += j;
+            }
+        }
+        return -1;
+    }
+};
