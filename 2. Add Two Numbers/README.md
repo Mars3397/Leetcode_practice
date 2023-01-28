88cfdc5d5ddc99f2f020f218f6f4d5eb9d62bb82
Mars <mars3397@gmail.com>
2023-01-29 01:18:18 +0800

    2023-01-29

---
+++ 2. Add Two Numbers/README.md
@@ -0,0 +1,4 @@
+### Note
+[題目](https://leetcode.com/problems/add-two-numbers/description/)
+
+- 新的值要放在下一個不然會讓最後多一個 ListNode
---
+++ 2. Add Two Numbers/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(0);
+        ListNode* current = head;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int x = l1 ? l1->val : 0;
+            int y = l2 ? l2->val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
+        }
+        return head->next;
+    }
+};
